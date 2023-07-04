43991e45b4a2974aa216ecd35129faf4ba5b2e40
brainstromsurfer <wishmeheaven@gmail.com>
2023-07-04 19:11:20 +0300

    lunching js

--- 1.1_primitive_data_types/1.1_primitive_data_types.js
+++ 1.1_primitive_data_types/1.1_primitive_data_types.js
@@ -1,7 +1,22 @@
 
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:brainstormsurfer/javascript.git
-git push -u origin main
+let massage = "You enter a dimly light room"
+
+let treasureCount = 2   
+
+let key = { type: "golden key with intricate carvings" }
+
+let isTrapActivated = false
+
+let secretPassage = null
+
+let mysteriousArtifact = undefined
+
+let ancientSymbol = Symbol("shekerkolshehu")
+
+let printTypesArr = [ massage, treasureCount, key , isTrapActivated, secretPassage, mysteriousArtifact, ancientSymbol ]
+
+console.log(`types of the variables above are: `)
+
+for (let i = 0 ; i <= printTypesArr.length ; i++) {    
+    console.log(`${i}. ${typeof(printTypesArr[i])} `);
+}
