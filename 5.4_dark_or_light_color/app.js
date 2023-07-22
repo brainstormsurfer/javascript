93c4edda0eaa79274a5ea3808a8ec4986ddd51d5
brainstromsurfer <wishmeheaven@gmail.com>
2023-07-23 00:38:19 +0300

    js_exercises_weekend

---
+++ 5.4_dark_or_light_color/app.js
@@ -0,0 +1,17 @@
+const darkOrLight = (color) => {  
+
+    switch (color.toLowerCase()) {
+      case 'yellow':
+         case 'pink':
+          case 'orange':
+      return 'light color'; 
+      case 'blue':
+        case 'purple':
+          case 'brown':
+      return 'dark color';
+      default:
+        return 'unknown color'
+    }
+}
+
+console.log(darkOrLight('OrAngE'));
