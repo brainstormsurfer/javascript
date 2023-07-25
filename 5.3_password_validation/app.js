d39d89f0361122e8a5634c530976d64b7b53b53f
brainstromsurfer <wishmeheaven@gmail.com>
2023-07-25 15:22:37 +0300

    ex5.3_update-regex_and_if-instead-ternary

--- 5.3_password_validation/app.js
+++ 5.3_password_validation/app.js
@@ -1,12 +1,13 @@
-const checkPasswordStrength = (password) => {              
-      return password.length > 7 ? 
-      (new RegExp('[/A-Z/]').test(password)) ?
-      'Very Strong' :
-      'Strong' :
-      'Weak'
+const checkPasswordStrength = (password) => {
+  if (password.length > 7) {
+    if ('[/A-Z/]'.test(password)) {
+      return "Very Strong";
+    } else {
+      return "Strong";
+    }
+  } else {
+    return "Weak";
+  }
+};
 
-}
-
-console.log(
-  checkPasswordStrength('rasasasaN')
-);
+console.log(checkPasswordStrength("rasasasaN"));
